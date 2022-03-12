d6b91456018a2717c7499a97ee7b572ad9118ec4
Daniel <daniel.masorete@gmail.com>
2022-03-12 16:41:53 +0200

    fixed router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import { routes } from '@/router/routes';
 
 export const router = createRouter({
+  history: createWebHistory(),
   routes,
   scrollBehavior() {
     return new Promise((resolve) => {
